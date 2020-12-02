53f36f2827a64d4a8b60abdfb597212788d41160
Baron-von-Riedesel <japhethx@gmail.com>
2020-12-02 08:20:03 +0100

    source tree and installation separated

--- Src/MZSUPP/Getenv.asm
+++ Src/MZSUPP/Getenv.asm
@@ -1,56 +1,52 @@
 
 ;*** read environment into a buffer ***
 
-if ?FLAT eq  0
+	.386
+;	.MODEL SMALL, stdcall
 
-        .386
-;        .MODEL SMALL, stdcall
+	include jmppm32.inc
 
-		include jmppm32.inc
-
-_TEXT32	segment dword public 'CODE'
+_TEXT32 segment dword public 'CODE'
 
 ;*** returns size of environment ***
 
 _getenvironment proc stdcall uses esi edi ebx pStr:dword, maxsize:dword
 
 
-        push    es
-        mov     es, [__psp]
-        mov     es,es:[002Ch]
-        mov     ebx,es
-        xor     edi,edi
-        mov     ecx,edi
-        dec     ecx
-        mov     al,00
+	push	es
+	mov 	es, [__psp]
+	mov 	es,es:[002Ch]
+	mov 	ebx,es
+	xor 	edi,edi
+	mov 	ecx,edi
+	dec 	ecx
+	mov 	al,00
 @@:
-        repnz   scasb
-        scasb
-        jnz     @B          ;edi-> 01
-        inc     edi         ;edi-> 00
-        inc     edi         ;edi-> first char of pgmname
-        repnz   scasb
-                            ;edi = size environment
-        pop     es
+	repnz	scasb
+	scasb
+	jnz 	@B			;edi-> 01
+	inc 	edi 		;edi-> 00
+	inc 	edi 		;edi-> first char of pgmname
+	repnz	scasb
+						;edi = size environment
+	pop 	es
 
-        push    ds
-        mov     ds,ebx
-        mov     ecx,edi
-        mov     eax,edi
-        cmp     ecx,maxsize
-        jb      @F
-        mov     ecx,maxsize
+	push	ds
+	mov 	ds,ebx
+	mov 	ecx,edi
+	mov 	eax,edi
+	cmp 	ecx,maxsize
+	jb		@F
+	mov 	ecx,maxsize
 @@:
-        xor     esi,esi
-        mov     edi,pStr
-        rep     movsb
-        pop     ds
-        ret
+	xor 	esi,esi
+	mov 	edi,pStr
+	rep 	movsb
+	pop 	ds
+	ret
 _getenvironment endp
 
-_TEXT32	ends
+_TEXT32 ends
 
-endif
-
-        end
+	end
 
