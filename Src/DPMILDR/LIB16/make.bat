faf127b387454f987f2db76387da91d21b4682be
Baron-von-Riedesel <japhethx@gmail.com>
2020-12-07 12:29:55 +0100

    delete obj modules in DPMILDR/LIB16

--- Src/DPMILDR/LIB16/make.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-jwasm -c -nologo -Fl -Sg hmemset.asm
-jwasm -c -nologo -Fl -Sg lstrcat.asm
-jwasm -c -nologo -Fl -Sg lstrcpy.asm
-jwasm -c -nologo -Fl -Sg lstrlen.asm
-jwasm -c -nologo -Fl -Sg profstrg.asm
-jwlib -b -n ldr16.lib +hmemset.obj +lstrcat.obj +lstrcpy.obj +lstrlen.obj +profstrg.obj
